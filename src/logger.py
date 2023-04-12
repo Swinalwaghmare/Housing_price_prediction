67d9f1cc134a223fd56285970fbad93537657683
Swinalwaghmare <swinalwaghmare28@gmail.com>
2023-04-13 01:20:11 +0530

    logging file added

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,19 @@ import logging
 from datetime import datetime
 import os
 
+ROOT_FILE_PATH = 'F:\Ineuron\Project\gemstone_price_prediction'
 log_dir = 'logs'
 current_time_stamp = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-log_file_name = f"log_{current_time_stamp}.log"
-os.makedirs(log_dir,exist_ok=True)
+log_file = f'log_{current_time_stamp}.log'
+logs_path = os.path.join(ROOT_FILE_PATH,log_dir)
 
-log_file_path = os.path.join(log_dir,log_file_name)
+os.makedirs(logs_path,exist_ok=True)
+
+log_file_path = os.path.join(logs_path,log_file)
 
 logging.basicConfig(
     filename=log_file_path, 
-    filemode="w",
+    filemode='w',
     format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
-

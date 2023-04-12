f182172c1e38246cfadfc6f2f289bbb355e6bb23
Swinalwaghmare <swinalwaghmare28@gmail.com>
2023-04-13 01:43:42 +0530

    constant & config added

--- src/Constant/__init__.py
+++ src/Constant/__init__.py
@@ -11,8 +11,6 @@ DATA_DIR_KEY = 'train.csv'
 # Data_ingestion
 DATA_INGESTION_ARTIFACT = 'data_ingestion'
 
-INGESTED_RAW_DIR = 'raw_data'
-INGESTED_RAW_DIR_KEY = 'raw.csv'
-INGESTED_TRAIN_TEST_DIR = 'ingested_data'
+RAW_DATA_KEY = 'raw.csv'
 TRAIN_DATA_KEY = 'train.csv'
 TEST_DATA_KEY = 'test.csv'

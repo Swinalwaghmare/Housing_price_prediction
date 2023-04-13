174a72c290249b06b0b510329873b105f8cf7578
Swinalwaghmare <swinalwaghmare28@gmail.com>
2023-04-14 03:16:34 +0530

    Model trainer added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -6,10 +6,13 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
-    dt_obj = DataTransformation()
-    dt_obj.initiate_data_transformation(train_data,test_data)
+    ingestion_obj = DataIngestion()
+    train_data,test_data = ingestion_obj.initiate_data_ingestion()
+    transformation_obj = DataTransformation()
+    train_arr, test_arr,_ = transformation_obj.initiate_data_transformation(train_data,test_data)
+    model_trainer = ModelTrainer()
+    model_trainer.intiate_model_training(train_arr,test_arr)
     
